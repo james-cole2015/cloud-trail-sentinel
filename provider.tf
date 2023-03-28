5d5951e23d934302a2a681a82f95a9be2182d90f
brooks <brooks.houck@gmail.com>
2023-03-28 17:21:08 +0000

    updating provider config

--- provider.tf
+++ provider.tf
@@ -1,9 +1,31 @@
 terraform {
   cloud {
-    organization = "acn-cloud-security"
+    organization = "friends_of_fate_903"
 
     workspaces {
-      name = "Sentinel-Policy-Tests"
+      name = "cloud-trail-sentinel"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      environment = "development"
+      platform    = "terraform"
+      repo-name   = "cloud-trail-sentinel"
+      user        = "Razer"
     }
   }
 }
